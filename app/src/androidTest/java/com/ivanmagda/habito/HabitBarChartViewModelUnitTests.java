2b729f315be6a5c6fb49b50387a9e710a7655ddf
Pintér Benedek <bndkpntr@outlook.com>
2017-05-07 23:53:52 +0200

    Merge fix

--- app/src/androidTest/java/com/ivanmagda/habito/HabitBarChartViewModelUnitTests.java
+++ app/src/androidTest/java/com/ivanmagda/habito/HabitBarChartViewModelUnitTests.java
@@ -1,8 +1,53 @@
 package com.ivanmagda.habito;
 
-/**
- * Created by bndkp on 6.5.17.
- */
+import com.ivanmagda.habito.barchart.HabitoBarChartRange;
+import com.ivanmagda.habito.barchart.formatters.MonthAxisValueFormatter;
+import com.ivanmagda.habito.barchart.formatters.WeekDayAxisValueFormatter;
+import com.ivanmagda.habito.barchart.formatters.YearAxisValueFormatter;
+import com.ivanmagda.habito.models.Habit;
+import com.ivanmagda.habito.view.model.HabitoBarChartViewModel;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class HabitBarChartViewModelUnitTests {
+
+    private HabitoBarChartViewModel mViewModel;
+    private Habit mHabit;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {HabitoBarChartRange.DateRange.WEEK, WeekDayAxisValueFormatter.class},
+                {HabitoBarChartRange.DateRange.MONTH, MonthAxisValueFormatter.class},
+                {HabitoBarChartRange.DateRange.YEAR, YearAxisValueFormatter.class}
+        });
+    }
+
+    @Parameterized.Parameter
+    public HabitoBarChartRange.DateRange mInput;
+
+    @Parameterized.Parameter(1)
+    public Class mExpected;
+
+    @Before
+    public void init() {
+        mHabit = new Habit();
+        mViewModel = new HabitoBarChartViewModel(mHabit, mInput);
+    }
+
+    @Test
+    public void getXAxisFormatterReturnsCorrectClass() throws Exception {
+        Class actual = mViewModel.getXAxisFormatter().getClass();
+
+        assertEquals(mExpected, actual);
+    }
 }
--- app/src/androidTest/java/com/ivanmagda/habito/HabitDetailViewModelUnitTests.java
+++ app/src/androidTest/java/com/ivanmagda/habito/HabitDetailViewModelUnitTests.java
@@ -1,8 +1,55 @@
 package com.ivanmagda.habito;
 
-/**
- * Created by bndkp on 6.5.17.
- */
+import com.ivanmagda.habito.barchart.HabitoBarChartRange;
+import com.ivanmagda.habito.utils.HabitoDateUtils;
+import com.ivanmagda.habito.view.model.HabitDetailViewModel;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class HabitDetailViewModelUnitTests {
+
+    private static final SimpleDateFormat WEEK_FORMAT = new SimpleDateFormat("d MMM yyyy", Locale.getDefault());
+    private static final SimpleDateFormat MONTH_FORMAT = new SimpleDateFormat("d MMM yyyy", Locale.getDefault());
+    private static final SimpleDateFormat YEAR_FORMAT = new SimpleDateFormat("MMM yyyy", Locale.getDefault());
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {HabitoBarChartRange.DateRange.WEEK, formatDates(WEEK_FORMAT, HabitoDateUtils.getStartOfCurrentWeek(), HabitoDateUtils.getEndOfCurrentWeek())},
+                {HabitoBarChartRange.DateRange.MONTH, formatDates(MONTH_FORMAT, HabitoDateUtils.getStartOfCurrentMonth(), HabitoDateUtils.getEndOfCurrentMonth())},
+                {HabitoBarChartRange.DateRange.YEAR, formatDates(YEAR_FORMAT, HabitoDateUtils.getStartOfCurrentYear(), HabitoDateUtils.getEndOfCurrentYear())}
+        });
+    }
+
+    private HabitDetailViewModel mViewModel;
+
+    @Parameterized.Parameter
+    public HabitoBarChartRange.DateRange mInput;
+
+    @Parameterized.Parameter(1)
+    public String mExpected;
+
+    @Test
+    public void getDateRangeStringTest() {
+        mViewModel = new HabitDetailViewModel(mInput);
+        String actual = mViewModel.getDateRangeString();
+        assertEquals(mExpected, actual);
+    }
+
+    private static String formatDates(SimpleDateFormat dateFormat, long start, long end) {
+        return dateFormat.format(new Date(start))
+                + " - "
+                + dateFormat.format(new Date(end));
+    }
 }
